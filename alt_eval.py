bf7b5cfe35f69c856cffd8f0163d9bd4ed95f80f
vicp1062 <victoria.peng@wspdigital.com>
2021-04-14 16:56:47 +1000

    Export alt_eval results to json file

--- alt_eval.py
+++ alt_eval.py
@@ -17,10 +17,26 @@ def _eval(path_to_checkpoint_file, path_to_eval_lmdb_dir):
     print(f'Evaluate {path_to_checkpoint_file} on {path_to_eval_lmdb_dir}, results =')
     print(results)
 
+    model_version = get_model_version(path_to_checkpoint_file)
+    export_to_json(model_version, results)
+
+
+def get_model_version(path_to_checkpoint_file):
+    start = path_to_checkpoint_file.find("-")+1
+    end = path_to_checkpoint_file.find(".pth")
+    substring = path_to_checkpoint_file[start:end]
+    return substring 
+
+
+def export_to_json(model_version, data):
+    with open(f'../logs/evaluate/data-{model_version}', 'w') as f:
+        json.dump(data, f)
+
 
 def main(args):
     path_to_test_lmdb_dir = os.path.join(args.data_dir, 'test.lmdb')
     path_to_checkpoint_file = args.checkpoint
+    get_model_version(path_to_checkpoint_file)
     print('Start evaluate')
     _eval(path_to_checkpoint_file, path_to_test_lmdb_dir)
     print('Done')
